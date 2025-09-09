7989d1b877c44e52eaacf432b3f41e0bf0284f24
Jarol Cachay <jarol_ccis@hotmail.com>
2025-09-08 22:31:21 -0500

    first commit

---
+++ src/test/java/com/jcc/CrudWebflux/CrudWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcc.CrudWebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
